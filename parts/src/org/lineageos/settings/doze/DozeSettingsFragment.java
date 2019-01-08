92269e698ab459594b33738c9ef0df69392724c6
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-01-08 14:56:52 +0100

    msm8953-common: doze: Move DozeUtils.checkDozeService in an handler
    * There's no need to manually write to the gestures prefs
      as long we are sure the handler is run after we return true
      in the onPreferenceChangeListner
    
    Change-Id: I090c0c6c91267aff69c45365b95b5f5e26d07b07

--- parts/src/org/lineageos/settings/doze/DozeSettingsFragment.java
+++ parts/src/org/lineageos/settings/doze/DozeSettingsFragment.java
@@ -26,6 +26,12 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v14.preference.PreferenceFragment;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -33,11 +39,6 @@ import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
-import androidx.preference.Preference;
-import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.PreferenceCategory;
-import androidx.preference.PreferenceFragment;
-import androidx.preference.SwitchPreference;
 
 import org.lineageos.settings.R;
 
@@ -52,6 +53,8 @@ public class DozeSettingsFragment extends PreferenceFragment implements OnPrefer
     private SwitchPreference mPickUpPreference;
     private SwitchPreference mHandwavePreference;
     private SwitchPreference mPocketPreference;
+
+    private Handler mHandler = new Handler();
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -137,10 +140,9 @@ public class DozeSettingsFragment extends PreferenceFragment implements OnPrefer
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (DozeUtils.ALWAYS_ON_DISPLAY.equals(preference.getKey())) {
             DozeUtils.enableAlwaysOn(getActivity(), (Boolean) newValue);
-        } else {
-            DozeUtils.enableGesture(getActivity(), preference.getKey(), (Boolean) newValue);
         }
-        DozeUtils.checkDozeService(getActivity());
+
+        mHandler.post(() -> DozeUtils.checkDozeService(getActivity()));
 
         return true;
     }
