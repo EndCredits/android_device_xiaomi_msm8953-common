2206079f5fb3521d8429c8cb106eb37543b0dd96
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2019-12-10 15:37:37 +0000

    msm8953-common: prop: Enabled 1080p on YouTube
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -33,7 +33,8 @@ vendor.voice.conc.fallbackpath=deep-buffer \
 vendor.voice.path.for.pcm.voip=true \
 vendor.voice.playback.conc.disabled=true \
 vendor.voice.record.conc.disabled=false \
-vendor.voice.voip.conc.disabled=true
+vendor.voice.voip.conc.disabled=true \
+sys.display-size=1920x1080
 
 # Audio dynamic feature flags
 PRODUCT_PROPERTY_OVERRIDES += \
