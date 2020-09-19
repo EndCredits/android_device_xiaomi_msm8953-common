90e8d27984e0404578629e7659a408c7803eeaf0
Luchetto <lucchetto.tie@live.com>
2020-09-19 21:53:49 +0530

    msm8953-commn: Copy vndk-29 libc++ to vendor
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -387,6 +387,13 @@ PRODUCT_PACKAGES += \
     libgui_vendor \
     libstdc++.vendor
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so
+
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libc++.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libc++.so
+
 # Thermal
 PRODUCT_PACKAGES += \
     thermal.msm8953
