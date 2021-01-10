9bae41279e8feeedfb4aa73cce08d7aaaf9454cb
MASTERGUY <murtipipo@gmail.com>
2021-01-10 04:41:22 +0000

    Revert "msm8953-common: disable blur by default"
    This reverts commit f1b19564aca5232580199003142a9c33d9e0094f.

--- msm8953.mk
+++ msm8953.mk
@@ -22,6 +22,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# Board
+PRODUCT_USES_QCOM_HARDWARE := true
+PRODUCT_BOARD_PLATFORM := msm8996
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
