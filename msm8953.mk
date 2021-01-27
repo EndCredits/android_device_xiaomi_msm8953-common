a346e55da7e8893edf95eebe0c3044911943b8d5
laityts <laityts@gmail.com>
2021-01-27 16:14:26 +0800

    msm8953-common: rename prop.mk to vendor_prop.mk

--- msm8953.mk
+++ msm8953.mk
@@ -147,6 +147,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service
 
+# Device Properties
+-include $(LOCAL_PATH)/vendor_prop.mk
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
