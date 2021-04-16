6a246fb93a36ff112a9e960e04f9eb2bebe47df5
EndCredits <alicization.han@gmail.com>
2021-04-17 00:21:07 +0800

    msm8953-common: Dropped nonexistent files

--- msm8953.mk
+++ msm8953.mk
@@ -363,7 +363,6 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/Sna
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp_policy/mediaswcodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaswcodec.policy
 
 # Sensors
